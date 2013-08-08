318980e406f086ad8fb806ce3cb25178d277b1a6
John Boyle <boylejm@gmail.com>
2013-08-07 21:59:58 -0700

    tweak to display area on main screen. show the level name

--- gamestuff.py
+++ gamestuff.py
@@ -263,8 +263,9 @@ def render_all(Game):
 
     #show player stats
     render_bar(1, 1, data.BAR_WIDTH, 'HP', Game.player.fighter.hp, Game.player.fighter.max_hp(Game), libtcod.light_red, libtcod.darker_red, Game)
-    libtcod.console_print_ex(Game.panel, 1, 3, libtcod.BKGND_NONE, libtcod.LEFT, 'Dungeon level: ' + str(Game.player.level))
-    libtcod.console_print_ex(Game.panel, 1, 4, libtcod.BKGND_NONE, libtcod.LEFT, 'Turn: ' + str(Game.player.game_turns))
+    libtcod.console_print_ex(Game.panel, 1, 3, libtcod.BKGND_NONE, libtcod.LEFT, mapname(Game))
+    libtcod.console_print_ex(Game.panel, 1, 4, libtcod.BKGND_NONE, libtcod.LEFT, 'Dungeon level: ' + str(Game.player.level))
+    libtcod.console_print_ex(Game.panel, 1, 5, libtcod.BKGND_NONE, libtcod.LEFT, 'Turn: ' + str(Game.player.game_turns))
 
     #print the game messages, one line at a time
     y = 1
