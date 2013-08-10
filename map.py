948f61fd6175534dc349e982b05907144bfc4b49
John Boyle <boylejm@gmail.com>
2013-08-09 17:34:20 -0700

    turn off individual clans in map.py

--- map.py
+++ map.py
@@ -194,7 +194,7 @@ def place_objects(room, Game):
             monster.ai.owner    = monster
             monster.id          = str(monster.dungeon_level) + '.' + str(nextid)
             monster.name        = choice + ' (' + str(monster.id) + ')'
-            monster.fighter.clan        = monster.name
+            #monster.fighter.clan        = monster.name
             nextid+=1
             monster.fighter.set_fov(Game)
 
