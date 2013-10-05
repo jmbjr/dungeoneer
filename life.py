4d99442d655f1ef3e28029dbb953af2df7378687
John Boyle <boylejm@gmail.com>
2013-10-04 19:41:06 -0700

    added random number generator support. send a custom random number generator and it will use it for the sim.

--- life.py
+++ life.py
@@ -5,59 +5,92 @@ import entitydata
 import time
 
 class World(object):
-    def __init__(self, nwidth, nheight, alivechar, deadchar):
+    def __init__(self, nwidth, nheight, alivechar, deadchar,char_option, rndgen):
+        
         self.nwidth = nwidth
         self.nheight = nheight
         self.alive = alivechar
         self.dead = deadchar
+        self.char_option = char_option
         self.population =[]
+        self.generation = 0
+        self.rndgen = rndgen
 
         self.con = libtcod.console_new(self.nwidth,self.nheight)
 
         self.init_world()
 
     def init_world(self):
-        self.population = [[ flip_coin()
+        self.generation = 0
+        self.population = [[ flip_coin(self.rndgen)
         for yy in range(self.nheight) ]
             for xx in range(self.nwidth) ]
+
+    def check_stable(self):
+        MAX_POP = 125
+        num_unstable=0
+        for yy in range(self.nheight):
+            for xx in range(self.nwidth):
+                if self.population[xx][yy] > 2 and self.population[xx][yy] <=MAX_POP:
+                    num_unstable+=1
+
+        if num_unstable < 5 and self.generation >500:
+            self.init_world()    
 
     def get_world(self):
         libtcod.console_clear(self.con)
         for yy in range(self.nheight):        
             for xx in range(self.nwidth):
-                my_color=self.random_color()    
+                #my_color=self.random_color() 
+                my_color = self.get_color(self.population[xx][yy])
                 libtcod.console_set_default_foreground(self.con, my_color)
-                libtcod.console_print_ex(self.con, xx, yy, libtcod.BKGND_NONE, libtcod.LEFT, self.get_entity(self.population[xx][yy]))
+                libtcod.console_print_ex(self.con, xx, yy, libtcod.BKGND_NONE, libtcod.LEFT, self.get_entity(self.population[xx][yy], self.char_option))
         return self.con
 
-    def get_entity(self, entity):
+    def get_entity(self, entity, option):
+        Max_ASCII = 125
+        ASCII_offset = 23
 
-        if entity == 0:
-            return ' '
-        elif entity < 10:
-            return '.'
-        elif entity >=10 and entity < 20:
-            return ','
-        elif entity >=20 and entity < 30:
-            return '_'
-        elif entity >=30 and entity < 40:
-            return '-'
-        elif entity >=40 and entity < 50:
-            return '|'
-        elif entity >=50 and entity < 60:
-            return '+'
-        elif entity >=60 and entity < 70:
-            return 'x'
-        elif entity >=70 and entity < 80:
-            return '='
-        elif entity >=80 and entity < 90:
-            return '#'
-        elif entity >=90 and entity < 100:
-            return 'o'
-        else:
-            return '@'
+        if option is 'symbol':
+
+            if entity == 0:
+                return ' '
+            elif entity < 10:
+                return '.'
+            elif entity >=10 and entity < 20:
+                return ','
+            elif entity >=20 and entity < 30:
+                return '_'
+            elif entity >=30 and entity < 40:
+                return '-'
+            elif entity >=40 and entity < 50:
+                return '|'
+            elif entity >=50 and entity < 60:
+                return '+'
+            elif entity >=60 and entity < 70:
+                return 'x'
+            elif entity >=70 and entity < 80:
+                return '='
+            elif entity >=80 and entity < 90:
+                return '#'
+            elif entity >=90 and entity < 100:
+                return 'o'
+            else:
+                return '@'
+        elif option is 'ascii':
+            if entity == 0:
+                thechar = ' '
+            elif entity < 10:
+                thechar = entity
+            elif entity >=10 and entity <= Max_ASCII - ASCII_offset:
+                thechar =  chr(entity + ASCII_offset)
+            else:
+                thechar = chr(Max_ASCII + 1)
+
+            return str(thechar)
 
     def update(self):
+        self.generation+=1
         new_population = []
         new_population = [[ 0
             for yy in range(self.nheight) ]
@@ -92,11 +125,17 @@ class World(object):
 
         self.population = new_population
             
-    def random_cell(self):
-        if flip_coin() == 0:
-            return self.dead
-        else:
-            return self.alive
+    def get_color(self, code):
+        rr = 8
+        gg = 8 + code*2
+        bb = 8
+
+        if gg > 255:
+            rr = 128
+            gg = 255
+            bb = 128
+
+        return libtcod.Color(rr,gg,bb)
 
     def random_color(self):
         rr = libtcod.random_get_int(0,0,255)
@@ -164,10 +203,18 @@ nwidth = 100
 nheight = 60
 alivechar = '+'
 deadchar = ' '
+char_option = 'ascii'
 speed = .1
 inc = 0.01
 
-world = World(nwidth,nheight, alivechar, deadchar)
+# default generator
+default = libtcod.random_get_instance()
+# another random generator
+my_random = libtcod.random_new()
+# a random generator with a specific seed
+my_determinist_random = libtcod.random_new_from_seed(0xdeadbeef)
+
+world = World(nwidth,nheight, alivechar, deadchar, char_option, my_determinist_random)
 
 libtcod.console_set_custom_font('oryx_tiles3.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD, 32, 12)
 libtcod.console_init_root(nwidth, nheight, 'johnstein\'s Game of RogueLife!', False, libtcod.RENDERER_SDL)
@@ -194,10 +241,12 @@ while not libtcod.console_is_window_closed():
     if key.vk ==libtcod.KEY_DOWN:
         speed+=inc
     if key.vk == libtcod.KEY_RIGHT:
-        inc+=.05
+        inc+=.01
     if key.vk ==libtcod.KEY_LEFT:
-        inc-=.05
+        inc-=.01
 
+    if speed <0:
+        speed = .001
     #display world
     con_world = world.get_world()
     libtcod.console_blit(con_world, 0, 0, nwidth, nheight, 0, 0, 0)
@@ -208,3 +257,4 @@ while not libtcod.console_is_window_closed():
     #replace old population with new one
     time.sleep(speed)
     world.update()
+    world.check_stable()
