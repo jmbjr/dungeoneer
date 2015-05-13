5be18a939f1036706ce48d69500c3c5b671598ae
John Boyle <boylejm@gmail.com>
2015-05-13 07:32:43 +0100

    add gamedata.py

---
+++ gamedata.py
@@ -0,0 +1,4 @@
+#.............................................
+#EDITABLE ENTITIES GENERAL DATA
+ASCIIMODE         = True #use Object.char for graphics if True. use tilechar if False.
+GRAPHICSMODE      = 'curses' #curses, libtcod (TODO: should roll in ASCIIMODE into this as libtcod-console and libtcod-ascii)
