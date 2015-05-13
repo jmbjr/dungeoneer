3ceaa7516e3822b9a884c4393879aeaf07212b5c
John Boyle <boylejm@gmail.com>
2015-05-13 00:42:22 -0700

    fixup win32 for libtcod

--- libtcod/__init__.py
+++ libtcod/__init__.py
@@ -4,7 +4,7 @@ if sys.platform == 'darwin':
     from libtcod.osx import *
 elif sys.platform == 'linux2':
     from libtcod.linux import *
-elif sys.platform == 'windows':
+elif sys.platform == 'win32':
     from libtcod.windows import *
 else:
     raise ImportError('unknown os', sys.platform)
